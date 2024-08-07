cafd9494d9197790d7d19eff8de1d7965a007058
Sunny Jha <jhasunny550@gmail.com>
2024-08-07 15:25:04 +0530

    Fix project link

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -34,7 +34,7 @@ const ProjectCard = ({
           />
           <div className="absolute inset-0 flex justify-end card-img_hover ">
             <div
-              onclick={() => window.open(source_code_link, "_blank")}
+              onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
