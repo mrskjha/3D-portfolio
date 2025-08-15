d3df4bb94449ce62bd20139a0d8a6c1c5c718e45
Sunny Kumar <135733878+mrskjha@users.noreply.github.com>
2025-08-15 17:30:14 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ const Hero = () => {
         Hi, I'm <span className="text-[#915eff]">Sunny</span>
       </h1>
       <p className={`${styles.heroSubText} mt-2 text-white-100 text-[16px] sm:text-[20px]`}>
-       Empowering brands with custom-built, responsive websites<br className="sm:block hidden" />
-        and intuitive web applications tailored to their needs
+       Turning ideas into <br className="sm:block hidden" />
+       impact...
       </p>
     </div>
   </div>
