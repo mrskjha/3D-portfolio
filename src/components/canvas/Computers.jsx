5d39b2331052eb48b49fae0f70a9fdfef25c17ff
Sunny Jha <jhasunny550@gmail.com>
2026-01-29 00:59:30 +0530

    fix error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,48 +4,61 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("/desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      {/* Lighting */}
-      <hemisphereLight intensity={0.9} groundColor="black" />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
-
-      {/* Model */}
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.45 : 0.75}
-        position={isMobile ? [0, -1.5, 0] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
-
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
     setIsMobile(mediaQuery.matches);
 
-    const handleChange = (e) => setIsMobile(e.matches);
+    const handleChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
     mediaQuery.addEventListener("change", handleChange);
 
-    return () => mediaQuery.removeEventListener("change", handleChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
   }, []);
 
   return (
     <Canvas
-      frameloop="always"
+      frameloop='demand' 
       shadows
-      camera={{ position: [20, 3, 5], fov: 35 }}
-      gl={{ preserveDrawingBuffer: true }}
+      dpr={[1, 2]} 
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ 
+        preserveDrawingBuffer: true,
+        powerPreference: "high-performance", 
+        antialias: false 
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -62,6 +75,3 @@ const ComputersCanvas = () => {
 };
 
 export default ComputersCanvas;
-
-
-useGLTF.preload("/desktop_pc/scene.gltf");
