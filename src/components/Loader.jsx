b05940ad5ccb40f9805503ca3cae4bd66fb44a92
Sunny Jha <jhasunny550@gmail.com>
2026-01-29 01:03:46 +0530

    Refactor Loader component and enhance canvas loader styles with animation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,17 +2,30 @@ import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
   const { progress } = useProgress();
+  
   return (
-    <Html>
-      <span className="canvas-load"></span>
+    
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      
+      <span className="canvas-loader"></span>
       <p
         style={{
           fontSize: 14,
-          color: "#f1f1f1",
+          color: "#F1F1F1",
           fontWeight: 800,
           marginTop: 40,
         }}
       >
+       
         {progress.toFixed(2)}%
       </p>
     </Html>
